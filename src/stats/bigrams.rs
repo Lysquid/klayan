5df95f33ed7229fd5701dbce480607eff30bb44f
Lysquid <8axop2rj@anonaddy.com>
2025-05-07 19:59:27 -0700

    test sfb

--- src/stats/bigrams.rs
+++ src/stats/bigrams.rs
@@ -28,6 +28,7 @@ pub fn calc_sfb(
             let finger = Finger::from(key);
             if let Some(prev_finger) = prev_finger {
                 if finger == prev_finger {
+                    // TODO: SKU if same key
                     sfb.entry(bigram).and_modify(|f| *f += freq).or_insert(freq);
                     break;
                 }
@@ -36,4 +37,55 @@ pub fn calc_sfb(
         }
     }
     sfb
+}
+
+#[cfg(test)]
+#[rustfmt::skip] 
+mod tests {
+
+    use super::*;
+    use crate::kalamine::PhysicalKey::*;
+
+    #[test]
+    fn clac_sfb_simple() {
+        let sym_to_ks = HashMap::from([
+            ('e', vec![KeyE]),
+            ('r', vec![KeyR]),
+            ('d', vec![KeyD]),
+        ]);
+        let sfb = HashMap::from([
+            (['e','d'], 1.0),
+            (['d','e'], 2.0),
+        ]);
+        let not_sfb = HashMap::from([
+            (['e','r'], 3.0),
+            (['r','e'], 4.0),
+            (['r','d'], 5.0),
+            (['d','r'], 6.0),
+        ]);
+        let mut bigrams_freq = sfb.clone();
+        bigrams_freq.extend(not_sfb);
+        let result = calc_sfb(&sym_to_ks, &bigrams_freq);
+        assert_eq!(result, sfb);
+    }
+
+    #[test]
+    fn clac_sfb_deadkey() {
+        let sym_to_ks = HashMap::from([
+            ('d', vec![KeyD]),
+            ('é', vec![Quote, KeyE]),
+        ]);
+        let sfb = HashMap::from([
+            (['é','d'], 1.0),
+        ]);
+        let not_sfb = HashMap::from([
+            (['d','é'], 1.0),
+            (['é','é'], 1.0),
+        ]);
+        let mut bigrams_freq = sfb.clone();
+        bigrams_freq.extend(not_sfb);
+        let result = calc_sfb(&sym_to_ks, &bigrams_freq);
+        assert_eq!(result, sfb);
+    }
+
 }
